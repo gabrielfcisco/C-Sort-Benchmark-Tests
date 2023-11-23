af362ffb9fac2bed81e16409c92b91c331256bb0
SAMUEL VANINI <samuel.v@puccampinas.edu.br>
2023-11-23 14:29:27 -0300

    fix: ajuste na main para executar os algoritmos

--- src/main.c
+++ src/main.c
@@ -9,43 +9,121 @@
 #include "../include/quickSort.h"
 #include "../include/timSort.h"
 
-#define TAM_ARRAY 100000
-
 int main()
+
 {   
-    int n = TAM_ARRAY;
-    struct data *array = malloc(n*sizeof(struct data));
+    for (int i = 0; i<6; i++){
 
-    srand(0);
-
-    for(int i = 0; i < n; i++){
-        array[i].key = rand();
-        array[i].field = rand() %(RAND_MAX - 100 + 1) + 100;
+        inProcess(10000, i);
+        inProcess(50000, i);
+        inProcess(100000, i);
+        inProcess(500000, i);
+        inProcess(1000000, i);
     }
 
-    // printf("Vetor original:\n");
-    // for (int i = 0; i < n; i++)
-    // {
-    //     printf("key: %d, field: %f\n", array[i].key, array[i].field);
-    // }
-    printf("\nExecutando ordenacao...");
+   return 0;
+}
 
-    clock_t begin = clock();
+double inProcess(int n, int f) {
 
-    insertionSort(array, n);
+    struct data *array = malloc(n*sizeof(struct data));
 
-    clock_t end = clock();
-    double timeSpent = (double) (end - begin) / CLOCKS_PER_SEC ;
+    clock_t begin;
+    clock_t end;
 
-    // printf("\nVetor ordenado em ordem decrescente:\n");
-    // for (int i = 0; i < n; i++)
-    // {
-    //     printf("key: %d, field: %f\n", array[i].key, array[i].field);
-    // }
+    for (int i = 0; i < 10; i++)
+    {
+        srand(i*10);
 
-    printf("\nOrdenacao concluida!\n\nTempo para ordenação gasto:%lf\n", timeSpent);
+        for(int j = 0; j < n; j++){
+            array[j].key = rand();
+            array[j].field = rand() % (RAND_MAX - 100 + 1) + 100;
+        }
 
-    free(array);
+        // printf("Vetor original:\n");
+        // for (int i = 0; i < n; i++)
+        // {
+        //     printf("key: %d, field: %f\n", array[i].key, array[i].field);
+        // }
+        printf("\nExecutando ordenacao... [%d] Método [%d]", i+1, f);
 
+        switch (f) {
+
+            case 0:
+
+                begin = clock();
+            
+                insertionSort(array, n);
+
+                end = clock();
+
+            break;
+
+            case 1:
+
+                begin = clock();
+            
+                bubbleSort(array, n);
+
+                end = clock();
+
+            break;
+
+            case 2:
+
+                begin = clock();
+            
+                shellSort(array, n);
+
+                end = clock();
+
+            break;
+
+            case 3:
+
+                begin = clock();
+            
+                mergeSort(array, 0, n);
+
+                end = clock();
+
+            break;
+
+            case 4:
+
+                begin = clock();
+            
+                quickSort(array, 0, n, h);
+
+                end = clock();
+
+            break;
+
+            case 5:
+
+                begin = clock();
+            
+                timSort(array, n);
+
+                end = clock();
+
+            break;
+
+        }
+        
+        double timeSpent = (double) (end - begin) / CLOCKS_PER_SEC ;
+
+        // printf("\nVetor ordenado em ordem decrescente:\n");
+        // for (int i = 0; i < n; i++)
+        // {
+        //     printf("key: %d, field: %f\n", array[i].key, array[i].field);
+        // }
+
+        printf("\nOrdenacao concluida!\n\nTempo para ordenação gasto:%lf\n", timeSpent);
+
+        free(array);
+    }
     return 0;
+
+
 }
